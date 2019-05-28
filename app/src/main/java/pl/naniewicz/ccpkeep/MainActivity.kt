87422f343274c09a3ea77e1d09ddc63b6c7f1a92
Rafal Naniewicz <rafal.naniewicz@netguru.pl>
2019-05-28 16:40:56 +0200

    Initial commit

---
+++ app/src/main/java/pl/naniewicz/ccpkeep/MainActivity.kt
@@ -0,0 +1,13 @@
+package pl.naniewicz.ccpkeep
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
